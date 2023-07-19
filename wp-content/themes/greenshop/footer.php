b37610d920ac083294766392881b0457e3caa955
Wojtek <wojciech.progr@gmail.com>
2023-07-19 23:06:20 +0200

    Dodanie Boostrap 5.3

--- wp-content/themes/greenshop/footer.php
+++ wp-content/themes/greenshop/footer.php
@@ -1,1 +1,15 @@
+<footer>
+    <div class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12 col-lg-6">
+                    kolumna 1
+                </div>
+                <div class="col-sm-12 col-lg-6">
+                    kolumna 2
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
 </body>
--- wp-content/themes/greenshop/header.php
+++ wp-content/themes/greenshop/header.php
@@ -8,7 +8,9 @@
     <link rel="stylesheet" href="<?= GREENSHOP_THEME_URL ?>/style.css" />
     <link rel="stylesheet" href="<?= GREENSHOP_THEME_URL ?>/css/reset.css" />
     <link rel="stylesheet" href="<?= GREENSHOP_THEME_URL ?>/css/styles.css" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 
     <script src="<?= GREENSHOP_THEME_URL ?>/js/scripts.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
   </head>
 <body>
