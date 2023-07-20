dc2374f6b0e5084f896de90add58236ab0592074
Wojtek <wojciech.progr@gmail.com>
2023-07-20 20:27:46 +0200

    Dodawanie funkcji WP do Heada i body

--- wp-content/themes/greenshop/header.php
+++ wp-content/themes/greenshop/header.php
@@ -1,46 +1,61 @@
 <!DOCTYPE html>
-<html lang="pl-PL">
+<html lang="<?= language_attributes(); ?>">
 
 <head>
 
   <meta charset="<?php bloginfo('charset') ?>" />
+
+  <?php /* Sprawdzamy, czy podstrona jest wyszukiwarką. Jeśli tak, zabraniamy indeksowania treści. */?>
+  <?php if (is_search()): ?>
+    <meta name="robots" content="noindex, nofollow" />
+  <?php endif; ?>
+
   <meta name="viewport" content="width=device-width" />
 
   <title>greenshop.com</title>
 
   <link rel="icon" type="image/x-icon" href="<?= GREENSHOP_THEME_URL ?>/favicon.ico">
-
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
-
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Lora&display=swap" rel="stylesheet">
-
   <link rel="stylesheet" href="<?= GREENSHOP_THEME_URL ?>/style.css" />
   <link rel="stylesheet" href="<?= GREENSHOP_THEME_URL ?>/css/styles.css" />
 
   <script src="<?= GREENSHOP_THEME_URL ?>/js/scripts.js"></script>
-
   <script src="https://kit.fontawesome.com/fc9a6b4d3f.js" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz"
+    crossorigin="anonymous"></script>
+
+  <?php /* Informuje innych właścicieli witryn, że umieściłem link do ich treści. */?>
+  <link rel="pingback" href="<?= bloginfo('pingback_url'); ?>">
+
+  <?php /* Action hook - wykorzystują to wtyczki, dzięki funkcji nie trzeba edytować plików motywu. */?>
+  <?= wp_head(); ?>
 
 </head>
 
-<header>
+<?php /* Dopisuje klasy zaleznie od strony na której się znajdujemy, np. stylowanie elementów zależnie od tego, czy user jest zalogowany lub strona 404.  */?>
 
-  <div class="header">
-    <div class="container">
-      <div class="row">
-        <div class="col-sm-12 col-lg-6 header__column">
+<body <?= body_class(); ?>>
 
-          <div class="header__option">Opcja 1</div>
-          <div class="header__option">Opcja 2</div>
-          <div class="header__option">Opcja 3</div>
-          <div class="header__option">Opcja 4</div>
+  <header>
 
+    <div class="header">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12 col-lg-6 header__column">
+
+            <div class="header__option">Opcja 1</div>
+            <div class="header__option">Opcja 2</div>
+            <div class="header__option">Opcja 3</div>
+            <div class="header__option">Opcja 4</div>
+
+          </div>
         </div>
       </div>
     </div>
-  </div>
 
-</header>
+  </header>
