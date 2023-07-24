9f86f90c222369fb07b8cb219e7ea2894da8e3f6
Wojtek <wojciech.progr@gmail.com>
2023-07-24 21:18:26 +0200

    Implemented <section>, added new <section> for containing content

--- wp-content/themes/greenshop/index.php
+++ wp-content/themes/greenshop/index.php
@@ -1,5 +1,17 @@
 <?php get_header(); ?>
-<div class="mainContainer" style="background-image: url('<?= GREENSHOP_THEME_URL ?>/images/bricks.jpeg')">
+<section class="mainContainer" style="background-image: url('<?= GREENSHOP_THEME_URL ?>/images/bricks.jpeg')">
+
+</section>
+
+<section class="contentContainer">
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-3">
+                test
+            </div>
+        </div>
+    </div>
+</section>
 
 </div>
 <?php get_footer(); ?>
