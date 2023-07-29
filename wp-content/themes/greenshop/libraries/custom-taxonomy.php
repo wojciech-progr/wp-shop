cb531ca360236b061ea4bcd21839fbbb06e48adc
Wojtek <wojciech.progr@gmail.com>
2023-07-29 22:06:17 +0200

    Deleted cities and countries, added places

--- wp-content/themes/greenshop/libraries/custom-taxonomy.php
+++ wp-content/themes/greenshop/libraries/custom-taxonomy.php
@@ -5,41 +5,6 @@ add_action('init', 'greenshop_init_posttypes');
 function greenshop_init_posttypes()
 {
 
-    $countries_args = array(
-        'labels' => array(
-            'name' => 'Countries',
-            'singular_name' => 'Countries',
-            'all_items' => 'All countries',
-            'add_new' => 'Add new country',
-            'add_new_item' => 'Add new country',
-            'edit_item' => 'Edit country',
-            'new_item' => 'New country',
-            'view_item' => 'See the country',
-            'search_items' => 'Search country',
-            'not_found' => 'Countries not found',
-            'not_found_in_trash' => 'Countries not found in trash',
-            'parent_item_colon' => ''
-        ),
-        'public' => true,
-        'public_queryable' => true,
-        'show_ui' => true,
-        'query_bar' => true,
-        'rewrite' => true,
-        'capability_type' => 'post',
-        'hierarchical' => false,
-        'menu_position' => 5,
-        'supports' => array(
-            'title',
-            'editor',
-            'thumbnail',
-            'author',
-            'custom-fields'
-        ),
-        'has_archive' => true
-    );
-
-    register_post_type('countries', $countries_args);
-
     $fights_args = array(
         'labels' => array(
             'name' => 'Fights',
@@ -120,30 +85,30 @@ function greenshop_init_taxonomies()
 {
 
     register_taxonomy(
-        'cities',
-        array('countries', 'events'),
+        'Locations',
+        array('events', 'fights'),
         array(
-            'hierarchical' => false,
+            'hierarchical' => true,
             'labels' => array(
-                'name' => 'Cities',
-                'singular_name' => 'Cities',
-                'search_items' => 'Search cities',
-                'popular_items' => 'Popular cities',
-                'all_items' => 'All cities',
+                'name' => 'Locations',
+                'singular_name' => 'Locations',
+                'search_items' => 'Search locations',
+                'popular_items' => 'Popular locations',
+                'all_items' => 'All locations',
                 'edit_item' => 'Edit citiy',
-                'update_item' => 'Update cities',
-                'add_new_item' => 'Add new city',
-                'new_item_name' => 'New city name',
-                'separate_items_with_commas' => 'Separate cities with comas',
-                'add_or_remove_items' => 'Add or remove cities',
-                'choose_from_most_used' => 'Choose from most used cities',
-                'menu_name' => 'Cities'
+                'update_item' => 'Update locations',
+                'add_new_item' => 'Add new location',
+                'new_item_name' => 'New location name',
+                'separate_items_with_commas' => 'Separate locations with comas',
+                'add_or_remove_items' => 'Add or remove locations',
+                'choose_from_most_used' => 'Choose from most used locations',
+                'menu_name' => 'locations'
             ),
             'show_ui' => true,
             'update_count_callback' => '_update_post_term_count',
             'query_var' => true,
             'rewrire' => array(
-                'slug' => 'city'
+                'slug' => 'location'
             )
         )
     );
