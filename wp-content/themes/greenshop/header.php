13e1ec8598dbf076c6f0f467ad7d766e967dc171
Wojtek <wojciech.progr@gmail.com>
2023-07-31 03:31:32 +0200

    Added main html tag, deleted not used CSS, formated code

--- wp-content/themes/greenshop/header.php
+++ wp-content/themes/greenshop/header.php
@@ -8,7 +8,7 @@
   <?php if (is_search()): ?>
     <meta name="robots" content="noindex, nofollow" />
   <?php endif; ?>
-  
+
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
   <title>
@@ -16,6 +16,7 @@
   </title>
 
   <meta name="description" content="example description of site">
+  <link rel="pingback" href="<?= bloginfo('pingback_url'); ?>">
 
   <link rel="icon" type="image/x-icon" href="<?= GREENSHOP_THEME_URL ?>/images/favicon.ico">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
@@ -36,7 +37,6 @@
     crossorigin="anonymous"></script>
 
   <?php /* Informs another websites that i am linking to them */?>
-  <link rel="pingback" href="<?= bloginfo('pingback_url'); ?>">
 
   <?php /* Action hook - Plugins uses that, so they dont have to edit theme files directly. */?>
   <?= wp_head(); ?>
@@ -52,7 +52,8 @@
       <div class="row">
         <div class="col-md-12">
           <a class="navbar__logo" href="<?= esc_url(home_url()) ?>">
-            <img class="navbar__img" width="" height="" alt="Shop logo" src="<?= GREENSHOP_THEME_URL ?>/images/logo.png">
+            <img class="navbar__img" width="" height="" alt="Shop logo"
+              src="<?= GREENSHOP_THEME_URL ?>/images/logo.png">
           </a>
           <ul class="navbar__menu" aria-label="main menu">
             <li onclick="showSubElement(this)" class="navbar__option navbar__option--guitar">
@@ -69,12 +70,12 @@
             <li onclick="showSubElement(this)" class="navbar__option navbar__option--events">
               <strong>Events</strong>
               <ul class="navbar__submenu">
-                <li class="navbar__submenuOption">Germany <img width="" height="" class="navbar__submenu--flag" alt="German flag"
-                    src="<?= GREENSHOP_THEME_URL ?>/images/germany-flag.png"></li>
-                <li class="navbar__submenuOption">Poland <img width="" height="" class="navbar__submenu--flag" alt="Polish flag"
-                    src="<?= GREENSHOP_THEME_URL ?>/images/poland-flag.png"></li>
-                <li class="navbar__submenuOption">Austria <img width="" height="" class="navbar__submenu--flag" alt="Austrian flag"
-                    src="<?= GREENSHOP_THEME_URL ?>/images/austria-flag.png"></li>
+                <li class="navbar__submenuOption">Germany <img width="" height="" class="navbar__submenu--flag"
+                    alt="German flag" src="<?= GREENSHOP_THEME_URL ?>/images/germany-flag.png"></li>
+                <li class="navbar__submenuOption">Poland <img width="" height="" class="navbar__submenu--flag"
+                    alt="Polish flag" src="<?= GREENSHOP_THEME_URL ?>/images/poland-flag.png"></li>
+                <li class="navbar__submenuOption">Austria <img width="" height="" class="navbar__submenu--flag"
+                    alt="Austrian flag" src="<?= GREENSHOP_THEME_URL ?>/images/austria-flag.png"></li>
                 <li class="navbar__submenuOption">Whole world</li>
               </ul>
             </li>
--- wp-content/themes/greenshop/index.php
+++ wp-content/themes/greenshop/index.php
@@ -1,118 +1,120 @@
 <?php get_header(); ?>
-<section class="features">
-    <h2 class="features__heading">Why should You choose our store?</h2>
-    <ul class="features__list">
-        <li class="features__feature">
-            <div class="features__title">
-                Best prices
-            </div>
-            <p class="features__description">
-                <i>
-                    We never overprice our products so you can expand your collection of musical instruments
-                    at
-                    a reasonable price
-                </i>
-            </p>
-        </li>
-        <li class="features__feature">
-            <div class="features__title">
-                Enthusiasts by nature
-            </div>
-            <p class="features__description">
-                <i>
-                    The specialists working in our store are a team directly related to the world of music
-                </i>
-            </p>
-        </li>
-        <li class="features__feature">
-            <div class="features__title">
-                Years of service
-            </div>
-            <p class="features__description">
-                <i>
-                    Our brand has been built over the years, which resulted in the trust of hundreds of
-                    customers.
-                </i>
-            </p>
-        </li>
-        <li class="features__feature">
-            <div class="features__title">
-                Up-to-date blog
-            </div>
-            <p class="features__description">
-                <i>
-                    Instantly learn about the most interesting news from the world of music or deepen your
-                    knowledge
-                    of this subject.
-                </i>
-            </p>
-        </li>
-    </ul>
-</section>
-<section class="promoted">
-    <ul class="promoted__list">
-        <li class="promoted__title">Recent articles</li>
-        <li class="promoted__option"><a href="#" class="promoted__link">
-                New guitar type becomes best choice for young musicians</a>
-        </li>
-        <li class="promoted__option"><a href="#" class="promoted__link">
-                Drum technology is evolving rapidly after new research</a></li>
-        <li class="promoted__option promoted__option--last"><a href="#" class="promoted__link">
-                See all articles</a></li>
-    </ul>
-    <ul class="promoted__list">
-        <li class="promoted__title">Upcoming events</li>
-        <li class="promoted__option">
-            <span class="promoted__date">01.08.2023</span>
-            <a href="#" class="promoted__link">
-                Black Octopus - Vienna
-                <img width="" height="" class="promoted__flag" alt="Austrian flag"
-                    src="<?= GREENSHOP_THEME_URL ?>/images/austria-flag.png">
-            </a>
-        </li>
-        <li class="promoted__option">
+<main>
+    <section class="features">
+        <h2 class="features__heading">Why should You choose our store?</h2>
+        <ul class="features__list">
+            <li class="features__feature">
+                <div class="features__title">
+                    Best prices
+                </div>
+                <p class="features__description">
+                    <i>
+                        We never overprice our products so you can expand your collection of musical instruments
+                        at
+                        a reasonable price
+                    </i>
+                </p>
+            </li>
+            <li class="features__feature">
+                <div class="features__title">
+                    Enthusiasts by nature
+                </div>
+                <p class="features__description">
+                    <i>
+                        The specialists working in our store are a team directly related to the world of music
+                    </i>
+                </p>
+            </li>
+            <li class="features__feature">
+                <div class="features__title">
+                    Years of service
+                </div>
+                <p class="features__description">
+                    <i>
+                        Our brand has been built over the years, which resulted in the trust of hundreds of
+                        customers.
+                    </i>
+                </p>
+            </li>
+            <li class="features__feature">
+                <div class="features__title">
+                    Up-to-date blog
+                </div>
+                <p class="features__description">
+                    <i>
+                        Instantly learn about the most interesting news from the world of music or deepen your
+                        knowledge
+                        of this subject.
+                    </i>
+                </p>
+            </li>
+        </ul>
+    </section>
+    <section class="promoted">
+        <ul class="promoted__list">
+            <li class="promoted__title">Recent articles</li>
+            <li class="promoted__option"><a href="#" class="promoted__link">
+                    New guitar type becomes best choice for young musicians</a>
+            </li>
+            <li class="promoted__option"><a href="#" class="promoted__link">
+                    Drum technology is evolving rapidly after new research</a></li>
+            <li class="promoted__option promoted__option--last"><a href="#" class="promoted__link">
+                    See all articles</a></li>
+        </ul>
+        <ul class="promoted__list">
+            <li class="promoted__title">Upcoming events</li>
+            <li class="promoted__option">
+                <span class="promoted__date">01.08.2023</span>
+                <a href="#" class="promoted__link">
+                    Black Octopus - Vienna
+                    <img width="" height="" class="promoted__flag" alt="Austrian flag"
+                        src="<?= GREENSHOP_THEME_URL ?>/images/austria-flag.png">
+                </a>
+            </li>
+            <li class="promoted__option">
 
-            <span class="promoted__date">10.08.2023</span>
-            <a href="#" class="promoted__link">
-                Lost Sailor - Warsaw
-                <img width="" height="" class="promoted__flag" alt="Austrian flag"
-                    src="<?= GREENSHOP_THEME_URL ?>/images/poland-flag.png">
-            </a>
-            <a href="#" class="promoted__link">
-                In Desert We Trust - Berdlin
-                <img width="" height="" class="promoted__flag" alt="Austrian flag"
-                    src="<?= GREENSHOP_THEME_URL ?>/images/germany-flag.png">
-            </a>
-        </li>
-        <li class="promoted__option">
-            <span class="promoted__date">15.08.2023</span>
-            <a href="#" class="promoted__link">
-                Porcupine - Berdlin
-                <img width="" height="" class="promoted__flag" alt="Austrian flag"
-                    src="<?= GREENSHOP_THEME_URL ?>/images/germany-flag.png">
-            </a>
-        </li>
-    </ul>
-    <ul class="promoted__list">
-        <li class="promoted__title promoted__title--hot">
-            Hot offers
-        </li>
-        <li class="promoted__option">
-            <a href="#" class="promoted__link">
-                Jackson JS12 DKA Dinky AHFBBLK
-            </a>
-        </li>
-    </ul>
-    <ul class="promoted__list">
-        <li class="promoted__title">
-            Famous fights
-        </li>
-        <li class="promoted__option">
-            <a href="#" class="promoted__link">
-                Joe Satriani vs Steve Vai
-            </a>
-        </li>
-    </ul>
-</section>
+                <span class="promoted__date">10.08.2023</span>
+                <a href="#" class="promoted__link">
+                    Lost Sailor - Warsaw
+                    <img width="" height="" class="promoted__flag" alt="Austrian flag"
+                        src="<?= GREENSHOP_THEME_URL ?>/images/poland-flag.png">
+                </a>
+                <a href="#" class="promoted__link">
+                    In Desert We Trust - Berdlin
+                    <img width="" height="" class="promoted__flag" alt="Austrian flag"
+                        src="<?= GREENSHOP_THEME_URL ?>/images/germany-flag.png">
+                </a>
+            </li>
+            <li class="promoted__option">
+                <span class="promoted__date">15.08.2023</span>
+                <a href="#" class="promoted__link">
+                    Porcupine - Berdlin
+                    <img width="" height="" class="promoted__flag" alt="Austrian flag"
+                        src="<?= GREENSHOP_THEME_URL ?>/images/germany-flag.png">
+                </a>
+            </li>
+        </ul>
+        <ul class="promoted__list">
+            <li class="promoted__title promoted__title--hot">
+                Hot offers
+            </li>
+            <li class="promoted__option">
+                <a href="#" class="promoted__link">
+                    Jackson JS12 DKA Dinky AHFBBLK
+                </a>
+            </li>
+        </ul>
+        <ul class="promoted__list">
+            <li class="promoted__title">
+                Famous fights
+            </li>
+            <li class="promoted__option">
+                <a href="#" class="promoted__link">
+                    Joe Satriani vs Steve Vai
+                </a>
+            </li>
+        </ul>
+    </section>
+</main>
 
 <?php get_footer(); ?>
