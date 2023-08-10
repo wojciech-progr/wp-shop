e9a1d4ccfbe055c7a5415014988e50e1f5355e06
Wojtek <wojciech.progr@gmail.com>
2023-08-10 18:28:56 +0200

    Code formated with Prettier extension in vscode

--- wp-content/themes/greenshop/js/functions.js
+++ wp-content/themes/greenshop/js/functions.js
@@ -8,35 +8,27 @@ add more elements to first line of function if needed
 
 */
 function showSubElement(mainElement) {
+  var subElement = mainElement.getElementsByTagName("ul");
+  var subElementStyle = getComputedStyle(subElement[0]).getPropertyValue(
+    "display"
+  );
 
-    var subElement = mainElement.getElementsByTagName("ul");
-    var subElementStyle = getComputedStyle(subElement[0]).getPropertyValue("display");
+  if (subElementStyle == "none") {
+    // show element
+    mainElement.classList.add("showSubElement");
 
-    if (subElementStyle == 'none') {
+    adjustsubElementSize(mainElement, subElement[0]);
 
-        // show element
-        mainElement.classList.add("showSubElement");
-
-        adjustsubElementSize(mainElement, subElement[0]);
-
-        if (mainElement.offsetWidth > subElement[0].offsetWidth) {
-
-            adjustsubElementSize(mainElement.offsetWidth, subElement[0]);
-
-        }
-
-    } else {
-
-        // hide element
-        mainElement.classList.remove("showSubElement");
-
+    if (mainElement.offsetWidth > subElement[0].offsetWidth) {
+      adjustsubElementSize(mainElement.offsetWidth, subElement[0]);
     }
-
+  } else {
+    // hide element
+    mainElement.classList.remove("showSubElement");
+  }
 }
 
 // Adjusts size of subElement
 function adjustsubElementSize(mainElementWidth, subElement) {
-
-    subElement.style.width = mainElementWidth + 'px';
-
+  subElement.style.width = mainElementWidth + "px";
 }
