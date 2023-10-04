8bcc7957259878d17b387332091e3be0545857c5
Wojtek <wojciech.progr@gmail.com>
2023-10-05 00:45:53 +0200

    Login fix - default wordpress version has been set

--- wp-content/themes/greenshop/functions.php
+++ wp-content/themes/greenshop/functions.php
@@ -6,25 +6,3 @@ if (!defined('GREENSHOP_THEME_URL')) {
 }
 
 require_once dirname(__FILE__) . "/libraries/custom-taxonomy.php";
-
-// Filter & Function to rename the WordPress logout URL
-add_filter('logout_url', 'my_logout_page', 10, 2);
-function my_logout_page($logout_url)
-{
-    return home_url('/admin-login.php'); // The name of your new login file
-}
-// Filter & Function to rename Lost Password URL
-add_filter('lostpassword_url', 'my_lost_password_page', 10, 2);
-function my_lost_password_page($lostpassword_url)
-{
-    return home_url('/admin-login.php?action=lostpassword'); // The name of your new login file
-}
-// Filter & Function to rename the WordPress login URL
-add_filter('login_url', 'login_page', 10, 3);
-function my_login_page($login_url, $redirect, $force_reauth)
-{
-    $login_page = home_url('/admin-login.php'); // The name of your new login file
-    $login_url = add_query_arg('redirect_to', $redirect, $login_page);
-    return $login_url;
-}
-?>
