50b844780b55ba3a569631d9417fcce299fb41cc
Wojtek <wojciech.progr@gmail.com>
2023-10-09 02:51:34 +0200

    Homepage get your account section fronts

--- wp-content/themes/greenshop/index.php
+++ wp-content/themes/greenshop/index.php
@@ -1,106 +1,7 @@
 <?php get_header(); ?>
 <main>
-    <section class="features">
-        <h2 class="features__heading">Why should You choose our store?</h2>
-        <ul class="features__list">
-            <li class="features__feature">
-                <div class="features__title">
-                    Best prices
-                </div>
-                <p class="features__description">
-                    <i>
-                        We never overprice our products so you can expand your collection of musical instruments
-                        at
-                        a reasonable price
-                    </i>
-                </p>
-            </li>
-            <li class="features__feature">
-                <div class="features__title">
-                    Enthusiasts by nature
-                </div>
-                <p class="features__description">
-                    <i>
-                        The specialists working in our store are a team directly related to the world of music
-                    </i>
-                </p>
-            </li>
-            <li class="features__feature">
-                <div class="features__title">
-                    Years of service
-                </div>
-                <p class="features__description">
-                    <i>
-                        Our brand has been built over the years, which resulted in the trust of hundreds of
-                        customers.
-                    </i>
-                </p>
-            </li>
-            <li class="features__feature">
-                <div class="features__title">
-                    Up-to-date blog
-                </div>
-                <p class="features__description">
-                    <i>
-                        Instantly learn about the most interesting news from the world of music or deepen your
-                        knowledge
-                        of this subject.
-                    </i>
-                </p>
-            </li>
-        </ul>
-    </section>
-    <section class="opinions">
-        <h2 class="opinions__heading">Why did others trust us?</h2>
-        <div class="splide">
-            <div class="splide__track">
-                <ul class="splide__list">
-
-                    <?php
-                    $args = array(
-                        'number' => 9,
-                        'post_id' => 54,
-                        'orderby' => 'comment_ID'
-                    );
-                    $comments = get_comments(array('post_id' => 54));
-
-                    foreach ($comments as $comment) {
-                        ?>
-                        <li class="splide__slide">
-                            <div class="opinions__opinion">
-                                <p>
-                                    <?= get_avatar($comment->user_id, 100, $default = '', $alt = '', $args = array('class' => 'opinions__avatar')) ?>
-                                <div>
-                                    <q>
-                                        <?= $comment->comment_content; ?>
-                                    </q>
-                                    -&nbsp;
-                                    <?= $comment->comment_author ?>,
-                                    <?php
-
-                                    /* Informations about post */
-
-                                    $post = get_post($comment->post_id);
-                                    $post_date = date('d-m-y', strtotime($post->post_date));
-                                    $post_title = wp_trim_words($post->post_title, $num_words = 4, $more = ' [...]');
-                                    $post_link = get_permalink($comment->post_id, $leavename = false);
-
-                                    ?>
-                                    <a href="<?= $post_link ?>">
-                                        <?= $post_title ?>
-                                    </a>,
-                                    <?= $post_date ?>
-                                </div>
-                                </p>
-                            </div>
-                        </li>
-                    <?php } ?>
-
-                </ul>
-            </div>
-        </div>
-    </section>
     <section class="promoted">
+        <h2 class="promoted__heading">What's new</h2>
         <ul class="promoted__list">
             <li class="promoted__title">Recent articles</li>
             <li class="promoted__option">
@@ -184,6 +85,169 @@
                 </a>
             </li>
         </ul>
+    </section>
+    <section class="joinUs">
+        <h2 class="joinUs__heading"> Get Your free account now!</h2>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="joinUs__box">
+                        <h3 class="joinUs__heading">I am a customer</h3>
+                        <span class="joinUs__subHeading">Become a user now and fulfill your musical passion.</span>
+                        <div class="joinUs__benefits">
+                            <span class="joinUs__access">Users benefits:</span>
+                            <ul class="joinUs__benefits">
+                                <li>
+                                    buy new and used products at a good price
+                                </li>
+                                <li>
+                                    comment and share your opinions with others
+                                </li>
+                                <li>
+                                    acquire knowledge thanks to our blog
+                                </li>
+                                <li>
+                                    Let yourself be drawn into the musical passion
+                                </li>
+                                <li>
+                                    Join now
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="joinUs__box">
+                        <h3 class="joinUs__heading">I am a partner</h3>
+                        <span class="joinUs__subHeading">Become a user now and fulfill your musical passion.</span>
+                        <div class="joinUs__benefits">
+                            <span class="joinUs__access">Partner benefits:</span>
+                            <ul class="joinUs__benefits">
+                                <li>
+                                    put your products up for sale quickly
+                                </li>
+                                <li>
+                                    become an authorized partner of the Greenshop store
+                                </li>
+                                <li>
+                                    pay a small fee to discover premium options
+                                </li>
+                                <li>
+                                    make buyers love your products
+                                </li>
+                                <li>
+                                    announce your events to a huge audience
+                                </li>
+                                <li>
+                                    Join now
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="features">
+        <h2 class="features__heading">Why should You choose our store</h2>
+        <ul class="features__list">
+            <li class="features__feature">
+                <div class="features__title">
+                    Best prices
+                </div>
+                <p class="features__description">
+                    <i>
+                        We never overprice our products so you can expand your collection of musical instruments
+                        at
+                        a reasonable price
+                    </i>
+                </p>
+            </li>
+            <li class="features__feature">
+                <div class="features__title">
+                    Enthusiasts by nature
+                </div>
+                <p class="features__description">
+                    <i>
+                        The specialists working in our store are a team directly related to the world of music
+                    </i>
+                </p>
+            </li>
+            <li class="features__feature">
+                <div class="features__title">
+                    Years of service
+                </div>
+                <p class="features__description">
+                    <i>
+                        Our brand has been built over the years, which resulted in the trust of hundreds of
+                        customers.
+                    </i>
+                </p>
+            </li>
+            <li class="features__feature">
+                <div class="features__title">
+                    Up-to-date blog
+                </div>
+                <p class="features__description">
+                    <i>
+                        Instantly learn about the most interesting news from the world of music or deepen your
+                        knowledge
+                        of this subject.
+                    </i>
+                </p>
+            </li>
+        </ul>
+    </section>
+    <section class="opinions">
+        <h2 class="opinions__heading">Recent comments</h2>
+        <div class="splide">
+            <div class="splide__track">
+                <ul class="splide__list">
+
+                    <?php
+                    $args = array(
+                        'number' => 9,
+                        'orderby' => 'comment_ID'
+                    );
+                    $comments = get_comments(array('post_id' => 54));
+
+                    foreach ($comments as $comment) {
+                        ?>
+                        <li class="splide__slide">
+                            <div class="opinions__opinion">
+                                <p>
+                                    <?= get_avatar($comment->user_id, 100, $default = '', $alt = '', $args = array('class' => 'opinions__avatar')) ?>
+                                <div>
+                                    <q>
+                                        <?= $comment->comment_content; ?>
+                                    </q>
+                                    -&nbsp;
+                                    <?= $comment->comment_author ?>,
+                                    <?php
+
+                                    /* Informations about post */
+
+                                    $post = get_post($comment->post_id);
+                                    $post_date = date('d-m-y', strtotime($post->post_date));
+                                    $post_title = wp_trim_words($post->post_title, $num_words = 4, $more = ' [...]');
+                                    $post_link = get_permalink($comment->post_id, $leavename = false);
+
+                                    ?>
+                                    <a href="<?= $post_link ?>">
+                                        <?= $post_title ?>
+                                    </a>,
+                                    <?= $post_date ?>
+                                </div>
+                                </p>
+                            </div>
+                        </li>
+                        <?php
+                    }
+                    ?>
+
+                </ul>
+            </div>
+        </div>
     </section>
 </main>
 
