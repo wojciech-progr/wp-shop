0edf50cbb9c60dd6c342f038498fbefd06eb9b3a
Wojtek <wojciech.progr@gmail.com>
2023-10-10 14:50:10 +0200

    Loop for showing posts at events listing

--- wp-content/themes/greenshop/archive-events.php
+++ wp-content/themes/greenshop/archive-events.php
@@ -1,17 +1,33 @@
 <?php
-
 get_header();
-
 ?>
 
-Events!
-
-<?php print(GREENSHOP_THEME_URL); ?>
+Lorem Ipsum before posts loop
 
 <?php
+if (have_posts()):
+    while (have_posts()):
+        the_post(); ?>
 
+        <div id="<?php the_ID(); ?>" <?php post_class('one-post'); ?>>
+            <?php the_title(); ?>
+        </div>
+
+        <?php
+    endwhile;
+else:
+    ?>
+
+    Nie ma postów
+
+    <?php
+endif;
+?>
+
+Lorem Ipsum after posts loop
+
+<?php
 get_sidebar('events');
 
 get_footer();
-
 ?>
